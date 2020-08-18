2e54c3141b0d72dd05bd404e2f79434faf325b37
Arthur Vieira de Alcântar Brito <arthurbrito1100@gmail.com>
2020-08-17 23:53:15 -0300

    Web Coder

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+    
+});
+
+
+
+
+
